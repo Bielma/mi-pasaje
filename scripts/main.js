fe9f3cc1d7607d0ca22cc026c5e2a742b1b7b868
Antonio <thetonybl@gmail.com>
2021-02-09 08:20:23 -0600

    Recover-pass

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
 var valido = false;
 
 function goToLogin() {
-    history.pushState(null, "index", "index.html");
+    //history.pushState(null, "index", "index.html");
+    window.history.pushState("http://example.ca", "login", "index.html");
 }
+
+
+function goToRegister() {
+    window.history.pushState("http://example.ca", "Registro", "register.html");
+}
+
+function goToRecoverPass() {
+    history.pushState("http://example.ca", "Recuperar contraseña", "recover-pass.html");
+}
+
 
 function validar(event) {
     console.log()
