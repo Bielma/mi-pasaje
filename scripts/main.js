26c195dff8e45dd0a20a1dad121bca3cf42448f0
Antonio <thetonybl@gmail.com>
2021-02-08 21:05:31 -0600

    recovery pass form

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+var valido = false;
+
 function goToLogin() {
     history.pushState(null, "index", "index.html");
 }
+
+function validar(event) {
+    console.log()
+}
+document.addEventListener()
